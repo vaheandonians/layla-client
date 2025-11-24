8c42ea3057250476bef6abec81d94a68f2441069
Vahe Andonians <vahe@andonians.com>
2025-11-24 13:03:11 +0100

    new model added

--- layla_client/datamodels/layla_model.py
+++ layla_client/datamodels/layla_model.py
@@ -4,4 +4,5 @@ from enum import Enum
 class LaylaModel(Enum):
     DOC_TRF_0_9B_MULTI_V1_0_0_PROD = "doc_trf_0.9b_multi_v1.0.0_prod"
     DOC_TRF_3B_MULTI_V1_0_0_PROD = "doc_trf_3b_multi_v1.0.0_prod"
+    DOC_TRF_1_2B_MULTI_V1_0_0_PROD = "doc_trf_1.2b_multi_v1.0.0_prod"
     DOC_QWEN_3B_MULTI_V2_0_0_PROD = "doc_qwen_3b_multi_v2.0.0_prod"
